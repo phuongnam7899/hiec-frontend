6ec7d81b74550b7fa3d22a054fdaa6392a18898c
phuongnam7899 <phuongnam7899@gmail.com>
2020-02-14 12:39:17 +0700

    fix login bug

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -166,28 +166,26 @@ function HookSignIn(props) {
             backgroundButton: "#1ABC9C",
             ImageURL:"https://scontent.fhan3-3.fna.fbcdn.net/v/t1.15752-9/85055898_133191184536849_84730002418958336_n.png?_nc_cat=100&_nc_oc=AQnwnWSAzvLc8L7LR8mXIHInGK5jFTL3v_hvHd0yNvF4xjyZTP9nu4HbgOQgBFemcgk&_nc_ht=scontent.fhan3-3.fna&oh=3ee88d558f7ab401687844b396bf88a3&oe=5EB828F4"
         }
-    const submit =  (e)=>{        
+    const submit = async (e)=>{        
         e.preventDefault(); 
         if(email && password){
             try{
-            axios.post("http://localhost:1234/api/auth/sign-in",{
+            const response = await axios.post("/api/auth/sign-in",{
                 email:email,
                 password:password,
-            }).then((res) =>{
-                const data = res.data;
-                if(res.status === 200){
+            });
+            const data = response.data;
+                if(response.status === 200){
                     const userToken = data.token;
                     const userInfo = data.userInfo;
                     dispatch(addToken(userToken));
                     dispatch(saveUser(userInfo));
                     props.history.push("/")
                 }
-                else{
-                   
-                }
-            })
-
-            }catch(err){console.log(err)}
+            }
+            catch(err){
+                setIsNote(true)
+            }
         }
         else{
             // Missing email or password
