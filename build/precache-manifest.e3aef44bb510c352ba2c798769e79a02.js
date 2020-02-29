402e7ed33cc6ae67400288e4c8ff1aabe02e6725
phuongnam7899 <phuongnam7899@gmail.com>
2020-02-29 15:15:49 +0700

    build to deploy

---
+++ build/precache-manifest.e3aef44bb510c352ba2c798769e79a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198928c65581cea9efc9141199f8e6d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d9052319f67a81b8eb",
+    "url": "/static/css/2.1694efe6.chunk.css"
+  },
+  {
+    "revision": "173259f1c351a033040e",
+    "url": "/static/css/main.bbf0b113.chunk.css"
+  },
+  {
+    "revision": "a5d9052319f67a81b8eb",
+    "url": "/static/js/2.1ee7e1b3.chunk.js"
+  },
+  {
+    "revision": "45bd25ef180cd1e2049775eec461715d",
+    "url": "/static/js/2.1ee7e1b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "173259f1c351a033040e",
+    "url": "/static/js/main.f7223f59.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
